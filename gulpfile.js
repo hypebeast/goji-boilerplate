79c87cedcbb7d0b281ad0d3b61e1775df38776f3
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-06-29 20:47:11 +0200

    add initial gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var csso = require('gulp-csso');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+
+gulp.task('sass', function() {
+  gulp.src('public/stylesheets/style.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(csso())
+    .pipe(gulp.dest('public/stylesheets'));
+});
+
+gulp.task('compress', function() {
+  gulp.src([
+    'public/vendor/angular.js',
+    'public/vendor/*.js',
+    'public/app.js',
+    'public/services/*.js',
+    'public/controllers/*.js',
+    'public/filters/*.js',
+    'public/directives/*.js'
+  ])
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('templates', function() {
+  gulp.src('public/views/**/*.html')
+    .pipe(templateCache({ root: 'views', module: 'MyApp' }))
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('public/stylesheets/*.scss', ['sass']);
+  gulp.watch(['public/**/*.js', '!public/app.min.js', '!public/vendor'], ['compress']);
+});
+
+gulp.task('default', ['sass', 'compress', 'watch']);
